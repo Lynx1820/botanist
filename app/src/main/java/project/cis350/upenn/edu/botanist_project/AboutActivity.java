36e5da0d27d94a4bda281b899588a20a8db59583
Max <maxying@sas.upenn.edu>
2017-04-19 17:53:05 -0400

    Standardized some toolbar appearance

--- app/src/main/java/project/cis350/upenn/edu/botanist_project/AboutActivity.java
+++ app/src/main/java/project/cis350/upenn/edu/botanist_project/AboutActivity.java
@@ -1,15 +1,12 @@
 package project.cis350.upenn.edu.botanist_project;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 /**
  * Most of the content in the About Page comes from the XML. This class is mostly wrapper.
  *
- * TODO: get rid of back toolbar? It's a bit redundant.
  */
 public class AboutActivity extends AppCompatActivity {
     public static final int MenuActivity_ID = 1;
@@ -20,11 +17,6 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-
-    public void onGoBackButtonClick(View v) {
-        Intent i = new Intent(this, MenuActivity.class);
-        startActivityForResult(i, MenuActivity_ID);
     }
 
 }
