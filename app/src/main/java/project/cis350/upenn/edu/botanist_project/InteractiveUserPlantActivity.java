15a3376507031c0efb56b5d3a6a64046dbd383e6
John Bartlett <jfbart@gmail.com>
2017-04-19 17:26:45 -0400

    Clicking type links to species info

--- app/src/main/java/project/cis350/upenn/edu/botanist_project/InteractiveUserPlantActivity.java
+++ app/src/main/java/project/cis350/upenn/edu/botanist_project/InteractiveUserPlantActivity.java
@@ -1,6 +1,7 @@
 package project.cis350.upenn.edu.botanist_project;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,11 +9,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.List;
+
 public class InteractiveUserPlantActivity extends AppCompatActivity {
 
     private Plant plant;
     private ImageView plantImage;
-    private TextView plantDesc; // name and species
+    private TextView nameDesc; // name
+    private TextView typeDesc; // species
     private TextView plantReminders;
 
     @Override
@@ -30,11 +35,30 @@ public class InteractiveUserPlantActivity extends AppCompatActivity {
             plantImage.setImageResource(R.drawable.pun_pending);
         }
 
-        plantDesc = (TextView) findViewById(R.id.plantDescr);
+        nameDesc = (TextView) findViewById(R.id.nameDescr);
+        typeDesc = (TextView) findViewById(R.id.typeDescr);
         plantReminders = (TextView) findViewById(R.id.plantReminders);
 
-        plantDesc.setText("Name: " + plant.getName() + "\nType: " + plant.getType());
+        nameDesc.setText("Name: " + plant.getName());
+        typeDesc.setText("Type: " + plant.getType());
+        typeDesc.setPaintFlags(typeDesc.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
         plantReminders.setText(plant.lastWateredText());
+    }
+
+    public void onTypeDescClick(View v) {
+        try {
+            List<PlantInfo> info = PlantInfo.readInfoFromJson(getAssets().open("plant_info.json"));
+            for (PlantInfo p : info) {
+                if (p.name.equals(plant.getType())) {
+                    Intent i = new Intent(v.getContext(), PlantInfoActivity.class);
+                    i.putExtra("PLANT", p);
+                    v.getContext().startActivity(i);
+                }
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void onWaterPlantButtonClick(View v) {
